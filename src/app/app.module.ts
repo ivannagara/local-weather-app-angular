6fdf90e45334558c3f1e3b6fff38f299d082f786
ivan nagara <ivannagara@gmail.com>
2023-02-14 16:54:12 +0800

    feat: current weather service and ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentWeatherComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    appId: 'bf8024b6bc407df1b2922a00985eeac8',
+    baseUrl: 'http://',
+}
